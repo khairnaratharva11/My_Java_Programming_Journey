3fcf042965421a743b82f892b18a462cd31c86b2
Atharva Khairnar <khairnar.atharva11@gmail.com>
2026-06-17 22:41:13 +0530

    Add files via upload

---
+++ SelectionDemo1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class SelectionDemo1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iNo = 0;
+
+        System.out.println("Enter Your Age: ");
+        iNo = sobj.nextInt();
+
+        if(iNo >= 18)
+        {
+            System.out.println("You can Vote");
+        }
+        else
+        {
+            System.out.println("You cannot Vote");  
+        }
+
+    }
+}
